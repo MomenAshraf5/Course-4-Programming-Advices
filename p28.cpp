28342c54419e81903d1c74923f5b1c7cc33ddc10
Moamen Ashraf <moamenashraf533@gmail.com>
2026-04-16 23:20:49 +0200

    Fix enum value for Even in enNumberType and refactor CheckNumberType and SumOddNumber functions

--- p28.cpp
+++ p28.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-enum enNumberType {Odd = 1, Even = 2};
+enum enNumberType {Odd = 1, Even = 0};
 
 int ReadNumber()
 {
@@ -13,17 +13,42 @@ int ReadNumber()
     return Number;
 }
 
-enNumberType CheckNumberType()
+enNumberType CheckNumberType(int number)
 {
-    
+    int EvenOrOdd = number % 2;
+
+    if(EvenOrOdd == enNumberType::Even)
+        return enNumberType::Even;
+    else
+        return enNumberType::Odd;
 }
 
-void GetOddNumber(int number)
+int SumOddNumber(int number)
 {
+    int Sum;
+    if(CheckNumberType(number))
+    {
+        for (int counter = number; counter > 0; counter -= 2)
 
+            Sum += counter;
+            cout << Sum << endl;
+    }else
+
+        for (int counter = number - 1; counter > 0; counter -= 2)
+
+            Sum += counter;
+
+    return Sum;
+}
+
+void PrintSumOdd()
+{
+    int result = SumOddNumber(ReadNumber());
+    cout << result << endl;
 }
 
 int main()
 {
+    PrintSumOdd();
     return 0;
 }
