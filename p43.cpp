87c6b410b0fd3a84f1fcaf5adc2b2c21ccb8e4d9
Berlo <moamenashraf533@gmail.com>
2026-05-07 13:56:00 +0300

    Refactor time conversion functions in p43.cpp for improved accuracy and readability

--- p43.cpp
+++ p43.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+struct strTime 
+{
+    int TimeInDays, TimeInHours, TimeInMinutes, TimeInSeconds;
+};
+
 int ReadNumber(string messages)
 {
     int Number;
@@ -15,28 +20,35 @@ int ReadNumber(string messages)
     return Number;
 }
 
-int ConvertSecondsIntoDays(int seconds)
+int ConvertSecondsIntoDays(float seconds)
 {
     return seconds / 60 / 60 / 24;
 }
-int ConvertSecondsIntoHours(int seconds)
+int ConvertSecondsIntoHours(float seconds ,int days)
 {
-    return seconds / 60 / 60;
+
+    return (seconds - days * 60 * 60 * 24) / 60 / 60;
 }
-int ConvertSecondsIntoMinutes(int seconds)
+int ConvertSecondsIntoMinutes(float seconds, int days, int hours)
 {
-    return seconds / 60;
+    return (seconds - days * 60 * 60 *24 - hours * 60 * 60) / 60;
 }
+int GetRemainderSeconds(float seconds, int days, int hours, int minutes)
+{
+    return (seconds - days * 60 * 60 * 24 - hours * 60 * 60 - minutes * 60);
+}
+
 
 int main()
 {
-    int Seconds = ReadNumber("How many Seconds you took ?");
+    float Seconds = ReadNumber("How many Seconds you took ?");
     int Days = ConvertSecondsIntoDays(Seconds);
-    int Hours = ConvertSecondsIntoHours(Seconds);
-    int Minutes = ConvertSecondsIntoMinutes(Seconds);
+    int Hours = ConvertSecondsIntoHours(Seconds, Days);
+    int Minutes = ConvertSecondsIntoMinutes(Seconds, Days, Hours);
+    int RemainderSeconds = GetRemainderSeconds(Seconds, Days, Hours, Minutes);
 
     cout << endl;
-    cout << "The Time is " << Days << ":" << Hours << ":" << Minutes << ":" << Seconds << endl;
+    cout << "The Time is " << Days << ":" << Hours << ":" << Minutes << ":" << RemainderSeconds << endl;
 
     return 0;
 }
