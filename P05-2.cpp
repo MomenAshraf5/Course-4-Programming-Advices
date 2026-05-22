62e2865d9ea3d037f8df8e0c02120e829764a432
Berlo <moamenashraf533@gmail.com>
2026-05-22 04:27:33 +0300

    add number reading and reversing functionality in P05-2.cpp

--- P05-2.cpp
+++ P05-2.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
+int ReadNumber(string messages)
+{
+    int Number;
+
+    do{
+        cout << messages << endl;
+        cin >> Number;
+        if(Number <= 0)
+            messages = "Please enter a number greater than zero";
+    }while(Number <= 0);
+
+    return Number;
+}
+
+int CheckReminderZero(int userNumber)
+{
+    const int FixedDivide = 10;
+    int Reminder = userNumber % FixedDivide;
+    return Reminder;
+}
+
+void DisplayNumbersInReversed(int userNumber)
+{
+    const int count = to_string(userNumber).length();
+    
+    for (int counter = 0; counter < count; counter++)
+    {
+        cout << CheckReminderZero(userNumber) << endl;
+        userNumber = userNumber / 10;
+    }
+}
 int main()
 {
+    int UserNumber = ReadNumber("Enter a positive number");
+    DisplayNumbersInReversed(UserNumber);
+    
     return 0;
 }
